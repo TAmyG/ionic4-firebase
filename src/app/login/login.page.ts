435f7c4b6325268cd9cb0ab3b4f9f2e282dac4fa
TAmy <tamy.vivas@gmail.com>
2019-06-23 18:14:46 -0600

    feat: Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { User } from "../entity/user.class";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  user: User = new User();
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async onLogin() {
+    const user = await this.authService.onLogin(this.user);
+    if (user) {
+      console.log("Success Login");
+      this.router.navigateByUrl("/");
+    }
   }
-
 }
