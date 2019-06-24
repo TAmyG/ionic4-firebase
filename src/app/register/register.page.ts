389e43123ae0009b868189ceaf68daf3b49a3cb4
TAmy <tamy.vivas@gmail.com>
2019-06-23 18:03:32 -0600

    feat: register of user

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { User } from "../entity/user.class";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"]
 })
 export class RegisterPage implements OnInit {
+  user: User = new User();
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  async onRegister() {
+    const user = await this.authService.onRegister(this.user);
+    if (user) {
+      console.log("Success, user created");
+      this.router.navigateByUrl("/");
+    }
   }
-
 }
